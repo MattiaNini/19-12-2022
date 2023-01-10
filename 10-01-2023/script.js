bbccc43ad94617eaad94a7a08c2e897bcbffb33f
MattiaNini <121028609+MattiaNini@users.noreply.github.com>
2023-01-10 17:28:23 +0100

    10-01-2023

---
+++ 10-01-2023/script.js
@@ -0,0 +1,19 @@
+//Esercizio 1
+const heroSection = () => {
+  const bodyEl = document.body;
+  const heroEl = document.createElement("section");
+  heroEl.className("hero");
+
+  const heroTitle = document.createElement("h1)");
+  heroTitle.className("hero__title");
+
+  const heroSubTitle = document.createElement("img");
+  heroImg.className("hero__img");
+  heroImg.setAttribute("src");
+
+  heroEl.appendChild(heroImg, heroTitle, heroSubtitle);
+  bodyEl.appendChild(heroEl);
+};
+
+console.log(heroEl);
+//AVANZATO
