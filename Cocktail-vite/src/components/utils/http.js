a95acc063a04953d01e295a5c8829252e27981e2
MattiaNini <121028609+MattiaNini@users.noreply.github.com>
2023-03-28 16:29:42 +0200

    Delete Cocktail-vite directory

--- Cocktail-vite/src/components/utils/http.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1";
-
-export const GET = async (path) => {
-  const res = await fetch(BASE_URL + path);
-  const data = await res.json();
-
-  return data;
-};
